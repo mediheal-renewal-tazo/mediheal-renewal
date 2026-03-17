f30b66c6219da1183c1be06663f0fe05b796b208
waterbeenn <beenn09@naver.com>
2026-03-17 10:53:05 +0900

    config.js 파일 추가

---
+++ config.js
@@ -0,0 +1,3 @@
+const isLocal = window.location.hostname === 'localhost';
+console.log(isLocal);
+export const KAKAO_REDIRECT_URI = isLocal ? 'http://localhost:5173/oauth/' : 'https://https://mediheal-tazo.vercel.app/oauth/';
